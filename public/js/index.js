b7bae73d611f7d6adb0696036400c5a0c9260cdd
Stefan Alexander Meng <alex_stefan_92@hotmail.com>
2012-05-31 02:07:34 -0430

    Front almost ready

---
+++ public/js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+  // fix navigation on scroll
+  var win = $(window)
+    , nav = $('#navigation')
+    , navTop = $('#navigation').length && $('#navigation').offset().top - 0
+    , isFixed = 0
+
+  processScroll()
+  
+  nav.on('click', function () {
+    if (!isFixed) setTimeout(function () {  win.scrollTop(win.scrollTop() - 200) }, 10)
+  })
+
+  win.on('scroll', processScroll)
+
+  function processScroll() {
+    var i, scrollTop = win.scrollTop()
+    if (scrollTop >= navTop && !isFixed) {
+      isFixed = 1
+      nav.addClass('navigation-fixed')
+    } else if (scrollTop <= navTop && isFixed) {
+      isFixed = 0
+      nav.removeClass('navigation-fixed')
+    }
+  }
+});
