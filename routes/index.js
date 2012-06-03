a0594b4c05d395fe242356f92d8af45257436371
sadasant <danielr@geekli.st>
2012-06-03 00:18:22 -0430

    todos

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-var nodemailer = require('nodemailer');
+var mailer = require('nodemailer');
 
 exports.index = function(req, res) {
   res.render('index', { title: 'CNIT' })
@@ -17,7 +17,83 @@ exports.us = function(req, res) {
 
 exports.sendEmail = function(req,res) {
 
+  // TODO:
+  //  According to the issue: <https://github.com/sadasant/CNIT.org.ve/issues/1>
+  //  the form will sent to this route the following body elements:
+  //    · name
+  //    · lastName
+  //    · email
+  //    · identityCard
+  //    · organization
+  //    · city
+  //    · type_of_payment
+  //    · payment_number
+  //    · payment_date
+  //    · payment_ammount
+  //    · n_tickets
+  //
+  //  So, req.body should have each one of them...
+
   var user = req.body.user
+
+  // TODO:
+  //  Once that we've stored each of the received body elements
+  //  we should validate them, like this:
+  //
+  //  if (!mail.search(validMail)) {
+  //    return res.send({ error : "Invalid Mail" })
+  //  }
+  //
+  //  Where `validMail` is a RegEx, for example.
+  //  Usually regexes will do all this dirty job,
+  //  so this is a plus thing, it's time to learn regexes! :D
+
+  // TODO:
+  //  Now that you have confirmed that the data is valid,
+  //  it's time to create the random value, this is less important right now, so just
+  //  save a string like:
+  //    var randomValue = 'this will be random after the rest is done'
+
+  // TODO:
+  //  then you can make the transport object...
+  //  to make sure our users and passwords are not
+  //  available to the public, you can include
+  //  a secret.js file on the top of this file
+  //  that secret could be similar to this file:
+  //    <https://gist.github.com/2859635#file_secret.js>
+  //
+  //  So here we could do like:
+  //    service : secret.service // haha, awesome!
+
+  // TODO:
+  //  So far so good, now it's time to send the freaking mail.
+  //  Express let you render partial views, like this:
+  //
+  //    var data = {
+  //    }
+  //
+  //    res.partial('email/new_request', data, renderedPartial)
+  //
+  //    function renderedPartial(err, body) {
+  //      if (err) {
+  //        console.log(err)
+  //        return res.send({ error : "D:" }) // I kid, write a better error x_x
+  //      }
+  //
+  //      var options = {
+  //        /* stuff here */
+  //        html : body
+  //      }
+  //
+  //      smptTransport.sendMail(options, mailSent)
+  //    })
+  //
+  //    // etc...
+  //
+  //  As you can see, we received a `body` with the html
+  //  using `res.partial` :)
+  //  yey!! \o/
+  //
 
   var transportObject = {
     service : "Gmail"
@@ -27,7 +103,7 @@ exports.sendEmail = function(req,res) {
     }
   }
 
-  var smptTransport = nodemailer.createTransport("SMTP", transportObject);
+  var smptTransport = mailer.createTransport("SMTP", transportObject);
 
   var mailOptions = {
     from    : "examp@CNIT.com"
