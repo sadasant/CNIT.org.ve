d891eaeb71f21731738ef965be4b54f8407e3d0c
Adrian Obelmejias <obeladrian@gmail.com>
2012-06-02 23:34:54 -0430

    Form Backend

--- routes/index.js
+++ routes/index.js
@@ -15,21 +15,21 @@ exports.us = function(req, res) {
   res.render('us', { title: 'CNIT - Nuestro equipo' })
 }
 
-exports.sendEmail = function(req,res){
-  var smptTransport  = nodemailer.createTransport("SMTP",{
-  	service: "Gmail",
+exports.sendEmail = function(req,res) {
+  var smptTransport  = nodemailer.createTransport("SMTP", {
+    service: "Gmail",
   	auth: {
   		user: "CNIT@cnit.com",
   		pass: "ab123456"
   	}
   });
   var mailOptions = {
-  	from   : "examp@CNIT.com",
-  	to     : "obeladrian@gmail.com",
-  	subject: "Mensaje Desde Node",
-  	text   : "Dummy text"
+    from   : "examp@CNIT.com"
+  , to     : "obeladrian@gmail.com"
+  , subject: "Mensaje Desde Node"
+  ,	text   : "Dummy text"
   }
-  smptTransport.sendMail(mailOptions,function(err,res){
+  smptTransport.sendMail(mailOptions,function(err,res) {
   	err ? console.log(error) : console.log("Message Sent: "+res.message);
 
   });
