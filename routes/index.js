d9aa93a9c279438cfd4f9ef75d28980623593bde
sadasant <danielr@geekli.st>
2012-06-02 23:54:45 -0430

    code style fixes

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,48 @@
 var nodemailer = require('nodemailer');
-/*
- * GET home page.
- */
 
 exports.index = function(req, res) {
   res.render('index', { title: 'CNIT' })
 };
 
+
 exports.participate = function(req, res) {
   res.render('participate', { title: 'CNIT - ¡Participa!' })
 }
+
 
 exports.us = function(req, res) {
   res.render('us', { title: 'CNIT - Nuestro equipo' })
 }
 
-exports.sendEmail = function(req,res) {
-  var smptTransport  = nodemailer.createTransport("SMTP", {
-    service: "Gmail",
-  	auth: {
-  		user: "CNIT@cnit.com",
-  		pass: "ab123456"
-  	}
-  });
-  var mailOptions = {
-    from   : "examp@CNIT.com"
-  , to     : "obeladrian@gmail.com"
-  , subject: "Mensaje Desde Node"
-  ,	text   : "Dummy text"
-  }
-  smptTransport.sendMail(mailOptions,function(err,res) {
-  	err ? console.log(error) : console.log("Message Sent: "+res.message);
 
-  });
-  console.log(req.body.user);
-  res.redirect('back');
+exports.sendEmail = function(req,res) {
+
+  var user = req.body.user
+
+  var transportObject = {
+    service : "Gmail"
+  , auth    : {
+      user  : "CNIT@cnit.com"
+    , pass  : "ab123456"
+    }
+  }
+
+  var smptTransport = nodemailer.createTransport("SMTP", transportObject);
+
+  var mailOptions = {
+    from    : "examp@CNIT.com"
+  , to      : "obeladrian@gmail.com"
+  , subject : "Mensaje Desde Node"
+  , text    : "Dummy text"
+  }
+
+  smptTransport.sendMail(mailOptions, mailSent)
+
+  function mailSent(err,res) {
+    if (err) return console.log(error)
+    console.log("Message Sent: "+res.message);
+  }
+
+  console.log(user)
+  res.redirect('back')
 }
